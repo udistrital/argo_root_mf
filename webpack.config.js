10e0b86814a62f415bdcfc803d7dd39cf506193d
Mauricio Alvarado <163943201+mau-oati@users.noreply.github.com>
2024-06-20 01:58:49 +0200

    feat: Update sass rules udistrital/argo_documentacion/issues/46

--- webpack.config.js
+++ webpack.config.js
@@ -27,21 +27,29 @@ module.exports = (webpackConfigEnv, argv) => {
     ],
   });*/
 
-return merge(defaultConfig, {
-  // modify the webpack config however you'd like to by adding to this object
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: "src/index.ejs",
-      templateParameters: {
-        isProd: webpackConfigEnv && webpackConfigEnv.isProd,
-        isDev: webpackConfigEnv && webpackConfigEnv.isDev,
-        isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
-        orgName,
-      },
-      favicon: "./src/favicon.ico",
-    }),
-    new DefinePlugin({
+  return merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isProd: webpackConfigEnv && webpackConfigEnv.isProd,
+          isDev: webpackConfigEnv && webpackConfigEnv.isDev,
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+        favicon: "./src/favicon.ico",
+      }),
+      new DefinePlugin({
       isProd: webpackConfigEnv && webpackConfigEnv.isProd,
       isDev: webpackConfigEnv && webpackConfigEnv.isDev,
       isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
