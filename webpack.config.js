aeee2903ad1cfbb5aa61608800a93b7a026c5707
Mauricio Alvarado <163943201+mau-oati@users.noreply.github.com>
2024-06-20 02:07:55 +0200

    feat: Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,14 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
